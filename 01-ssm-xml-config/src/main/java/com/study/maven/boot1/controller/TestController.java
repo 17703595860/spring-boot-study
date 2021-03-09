69880f4dcdfd6674b009d77c9d8b6f800aa58d00
helinhai <17703595860@163.com>
2021-03-09 20:14:03 +0800

    ssm xml方式配置

---
+++ 01-ssm-xml-config/src/main/java/com/study/maven/boot1/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.study.maven.boot1.controller;
+
+import com.study.maven.boot1.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author HLH
+ * @email 17703595860@163.com
+ * @date : Created in  2021/3/9 19:59
+ */
+@Controller
+public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "success";
+    }
+
+    @GetMapping("/test2")
+    @ResponseBody
+    public String test2() {
+        return testService.getString();
+    }
+
+    @GetMapping("/test3")
+    public String test3() {
+        return "index";
+    }
+
+}
---
+++ 01-ssm-xml-config/src/main/java/com/study/maven/boot1/service/TestService.java
@@ -0,0 +1,17 @@
+package com.study.maven.boot1.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author HLH
+ * @email 17703595860@163.com
+ * @date : Created in  2021/3/9 20:09
+ */
+@Service
+public class TestService {
+
+    public String getString() {
+        return "success service";
+    }
+
+}
